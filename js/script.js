c2afe69b53facacfa8053deab617db17eea3f872
angel <angelmeregildo@live.com>
2022-12-20 01:32:10 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("sticky", this.window.scrollY > 0)
+})
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
